71d7c07e27ed2562c0b6ab9fc699f11ff5c4b398
Abhi <Abhinav.indian007@gmail.com>
2023-03-28 21:44:41 +0530

    initial commit

---
+++ project.js
@@ -0,0 +1,14 @@
+//deposit some money
+//determine number of lines to bet on
+//collect a bet amount
+//spin the slot achine
+//check if the user won
+//give the money
+//play again
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount : ")
+
+}
+deposit();
